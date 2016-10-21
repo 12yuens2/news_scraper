1b8b111f358b6ce6da3859869f79667bdbd4603b
dR3am5t0rM <sizhe@hotmail.com>
2016-10-21 17:56:57 +0100

    Refactored crawler to use callback to get all articles as json.

--- server.js
+++ server.js
@@ -25,53 +25,32 @@ io.sockets.on('connection', function (socket) {
 });
 
 
+function do_request(init_url, base_url, a_class, t_class, p_class, callback) {
+	var articles = [];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// NEEDS TO BE REFACTORED!!!
-
-
-function process_nlp(sentence) {
-	sentence = nlp.sentence(sentence).replace("[Place]", "Lithuania").text();
-	sentence = nlp.sentence(sentence).replace("[Person]", "Martynas").text();
-	return nlp.sentence(sentence).replace("[Organization]", "University of St. Andrews").text();
-	// return nlp.text(sentence).terms();
-	// return nlp.noun("Colchester").is_place();
-}
-
-function do_request(init_url, base_url, a_class, t_class, p_class) {
-	console.log("NEWS FROM " + init_url);
-	console.log("-------------------------------------------")
 	request(init_url, function(error, response, body) {
-	if(!error) {
-		var page = cheerio.load(body);
-		var links = page(a_class);
-		var article = "";
-		links.each(function(i, link) {
-			var href = link["attribs"]["href"];
-			var url = base_url;
-			// console.log(link);
-			if (href.indexOf(url) >= 0) {
-				url = href;
-			} else {
-				url = url.concat(href);
-			}
-			// console.log(url);
+		if(!error) {
+			var page = cheerio.load(body);
+			var links = page(a_class);
 
-				request(url, function(error, response, body) {
-					if(!error) {
+			var callCount = links.length;
+
+			//go scrape each link
+			for (var i = 0; i < links.length; i++) {
+				var link = links[i];
+
+				var href = link["attribs"]["href"];
+				var url = base_url;
+
+				if (href.indexOf(url) >= 0) {
+					url = href;
+				} else {
+					url = url.concat(href);
+				}
+
+				//get text from each link
+				request(url, function (error, response, body) {
+					if (!error) {
 						var page = cheerio.load(body);
 						var text = page(p_class + " p").text();
 						var title = page(t_class).text();
@@ -80,37 +59,26 @@ function do_request(init_url, base_url, a_class, t_class, p_class) {
 						obj.title = title;
 						obj.body = text;
 						articles.push(obj);
+						--callCount;
+					}
 
-						// jsonfile.writeFile(file, articles, function(err) {
-						// 	// console.log(err);
-						// });
-						// if(text) {
-						// 	sentences.forEach(function(sentence) {
-						// 		article = article + sentence +"\n";
-						// 	});
-						// 	articles.push(article);
-						// 	console.log("pushed");
-							//console.log(JSON.stringify(articles));
-						// 	console.log("=======================")
-						// }
+					//callback when done with all links
+					if (callCount <= 0) {
+						callback(articles);
 					}
 				});
-			});
+			}
 		}
 	});
-	console.log("--------------------------------------------------")
 }
 
-function do_all(){
-	do_request("http://www.bbc.co.uk/news/uk", "http://www.bbc.co.uk", ".faux-block-link__overlay-link", ".story-body__h1", ".story-body__inner");
-	// do_request("https://www.theguardian.com/uk", "https://www.theguardian.com", ".u-faux-block-link__overlay", ".content__headline", ".content__article-body");
-	// do_request("http://www.reuters.com", "http://www.reuters.com", ".story-title a", "#article-text");
-	
 
+/**
+ * Makes requests to all links of all websites.
+ * @param callback function, the first parameter of the callback is the json array of articles/
+ */
+function do_all(callback){
+	do_request("http://www.bbc.co.uk/news/uk", "http://www.bbc.co.uk", ".faux-block-link__overlay-link", ".story-body__h1", ".story-body__inner", callback);
+	// do_request("https://www.theguardian.com/uk", "https://www.theguardian.com", ".u-faux-block-link__overlay", ".content__headline", ".content__article-body", callback);
+	// do_request("http://www.reuters.com", "http://www.reuters.com", ".story-title a", "#article-text", callback);
 }
-
-//do_all();
-// do_request("http://www.bbc.co.uk/news/uk", "http://www.bbc.co.uk", ".faux-block-link__overlay-link", ".story-body__inner");
-// do_request("https://www.theguardian.com/uk", "https://www.theguardian.com", ".u-faux-block-link__overlay", ".content__article-body");
-// do_request("http://www.reuters.com", "http://www.reuters.com", ".story-title a", "#article-text");
-// do_request("http://edition.cnn.com", "http://edition.cnn.com", ".cd__headline a", ".l-container")
