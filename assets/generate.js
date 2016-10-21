c355813bf01dccd58dfcb3f63e342e0a45c783ff
mtnoreika <mn55@st-andrews.ac.uk>
2016-10-21 23:30:50 +0100

    More improvements.

--- assets/generate.js
+++ assets/generate.js
@@ -1,15 +1,26 @@
 var socket = io(); // your initialization code here.
+var colSize = 231;
+
 
 socket.emit('generate');
 
 socket.on('response', function(res){
-    console.log("Heelo");
+    console.log(res[0].title);
 
-    for (var i = 1; i < 7; i++) {
+
+    for (var i = 1; i < 8; i++) {
         var col = document.getElementById("col" + i);
 
-        console.log("col" + i);
-
-        col.innerHTMl = "<a>Hey</a>";
+        col.innerHTML =
+            "<h4><b>" + res[i - 1].title + "</b></h4>" +
+            "<p>" + res[i - 1].body.substring(0, colSize - 3) + " ...</p>" +
+            "<p><a class='btn btn-default' href='#' role='button'>Read Article &raquo;</a></p>";
     }
 });
+
+
+function replaceLastWord(string) {
+    for (var i = 0; i < string.length; i++) {
+
+    }
+}
