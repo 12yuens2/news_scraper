aef192e9c05935400ba577ddb8dee76391dd4d72
mtnoreika <mn55@st-andrews.ac.uk>
2016-10-22 17:54:07 +0100

    Improved CSS. The website is now displayed properly on different window sizes.

--- assets/generate.js
+++ assets/generate.js
@@ -1,26 +1,35 @@
-var socket = io(); // your initialization code here.
+var socket = io();
 var colSize = 231;
 
 
 socket.emit('generate');
 
 socket.on('response', function(res){
-    console.log(res[0].title);
 
-
+    // Populating the HTML page with articles
     for (var i = 1; i < 8; i++) {
         var col = document.getElementById("col" + i);
 
         col.innerHTML =
             "<h4><b>" + res[i - 1].title + "</b></h4>" +
-            "<p>" + res[i - 1].body.substring(0, colSize - 3) + " ...</p>" +
+            "<p>" + getIntro(res[i - 1].body) + "</p>" +
             "<p><a class='btn btn-default' href='#' role='button'>Read Article &raquo;</a></p>";
+
+
     }
+
 });
 
+// Makes an short description of an article
+function getIntro(article) {
+    var intro = article.substring(0, colSize);
+    var index = intro.length;
 
-function replaceLastWord(string) {
-    for (var i = 0; i < string.length; i++) {
+    while (!/\s/.test(intro.charAt(index))) {
+        intro = intro.substring(0, index);
 
+        index--;
     }
+
+    return intro + "...";
 }
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Library imports
-var fs = require("fs");
 var request = require("request");
 var cheerio = require("cheerio");
 
