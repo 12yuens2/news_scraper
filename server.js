503a7bc4c2cdc9bd19b8b7d350caebdc41a9a138
dR3am5t0rM <sizhe@hotmail.com>
2016-10-22 17:21:50 +0100

    Writing to locally saved json file, updates every day at 9am.

--- server.js
+++ server.js
@@ -2,32 +2,36 @@
 var fs = require("fs");
 var request = require("request");
 var cheerio = require("cheerio");
+var schedule = require("node-schedule");
 
 
 // Server constants
-var port = 5000; // WebSocket connection port
-var i = 0;
-var s = 6;
-var initUrl = "http://www.bbc.co.uk/news/uk";
-var articles = [];
-var file = "articles.json";
+// var port = 5000; // WebSocket connection port
+// var i = 0;
+// var s = 6;
+// var initUrl = "http://www.bbc.co.uk/news/uk";
+// var articles = [];
+// var file = article_file;
+//
+// // Initialising the socket.io object which abstracts web sockets
+// var io = require('socket.io').listen(port);
+//
+// // Dealing with new user connection
+// io.sockets.on('connection', function (socket) {
+//
+//     // On generate request generating a customised site for the user and sending it to the client
+//     socket.on('generate', function () {
+//         get_new_articles(function(articles) {
+//             io.emit("response", articles);
+//         });
+//     });
+// });
 
-// Initialising the socket.io object which abstracts web sockets
-var io = require('socket.io').listen(port);
 
-// Dealing with new user connection
-io.sockets.on('connection', function (socket) {
+//Other constants
+var article_file = "articles.json";
 
-    // On generate request generating a customised site for the user and sending it to the client
-    socket.on('generate', function () {
-        do_all(function(articles) {
-            io.emit("response", articles);
-        });
-    });
-});
-
-
-function do_request(init_url, base_url, a_class, t_class, p_class, callback) {
+function get_article(init_url, base_url, a_class, t_class, p_class, callback) {
 	var articles = [];
 
 	request(init_url, function(error, response, body) {
@@ -74,13 +78,44 @@ function do_request(init_url, base_url, a_class, t_class, p_class, callback) {
 	});
 }
 
+function write_to_file(articles) {
+	//randomise articles found
+	articles.sort(function() {
+		return 0.5 - Math.random();
+	});
+
+	fs.open(article_file, "a+", function(err, fd) {
+		if (err) {
+
+		} else {
+			var articles_json = JSON.parse(fs.readFileSync(article_file));
+
+			//get first 4 articles and push to json
+			for (var i = 0; i<4; i++) {
+				articles_json.push(articles[i]);
+			}
+			articles_json = JSON.stringify(articles_json);
+			fs.writeFileSync(article_file, articles_json);
+		}
+	});
+
+}
 
 /**
- * Makes requests to all links of all websites.
- * @param callback function, the first parameter of the callback is the json array of articles/
+ * Truncates the article_file and fetches new articles to populate the file.
+ * @param callback function, the first parameter of the callback is the json array of articles from each site.
  */
-function do_all(callback){
-	do_request("http://www.bbc.co.uk/news/uk", "http://www.bbc.co.uk", ".faux-block-link__overlay-link", ".story-body__h1", ".story-body__inner", callback);
-	// do_request("https://www.theguardian.com/uk", "https://www.theguardian.com", ".u-faux-block-link__overlay", ".content__headline", ".content__article-body", callback);
-	// do_request("http://www.reuters.com", "http://www.reuters.com", ".story-title a", "#article-text", callback);
+function get_new_articles() {
+	//wipe old articles
+	fs.writeFileSync(article_file, "[]");
+
+	//fetch new articles from websites
+	get_article("http://www.bbc.co.uk/news/uk", "http://www.bbc.co.uk", ".faux-block-link__overlay-link", ".story-body__h1", ".story-body__inner", write_to_file);
+	get_article("http://www.theonion.com", "http://www.theonion.com", ".handler", ".content-header", ".content-text", write_to_file);
+	get_article("https://www.theguardian.com/uk", "https://www.theguardian.com", ".u-faux-block-link__overlay", ".content__headline", ".content__article-body", write_to_file);
 }
+
+var j =  schedule.scheduleJob({hour: 09, minute: 00}, function() {
+	console.log("fetching new articles...");
+	get_new_articles();
+});
