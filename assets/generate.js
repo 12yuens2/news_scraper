0d8d7b84f6c0fb5b40c7506afd2dd569386d86b8
mtnoreika <mn55@st-andrews.ac.uk>
2016-10-26 00:06:31 +0100

    CSS finished.

--- assets/generate.js
+++ assets/generate.js
@@ -17,6 +17,8 @@ if (typeof(Storage) !== "undefined") {
 socket.on('response', function(res){
     articles = [];
 
+    console.log(res);
+
     // Populating the HTML page with articles
     for (var i = 1; i < 13; i++) {
         var col = document.getElementById("col" + i);
@@ -27,9 +29,13 @@ socket.on('response', function(res){
         if (res[i - 1] === undefined)
             continue;
 
+        if (res[i - 1].person === undefined) {
+            continue;
+        }
+
         if (col.id === "col1" || col.id === "col2" || col.id === "col3" || col.id === "col4") {
             col.innerHTML =
-                '<img src="' + fbData[i - 1]['picture'].data.url + '" alt="Article picture" class="frontImg">' +
+                '<img src="' + res[i - 1].person + '" alt="Article picture" class="frontImg">' +
                 "<h4>" + res[i - 1].title + "</h4>" +
                 "<div class='frontText'>" + getIntro(res[i - 1].body) + "</div>" +
 		        "<a class='btn btn-default frontButton' onclick='updateArticle(" + i + ")' data-toggle='modal' data-target='#modal' role='button'>Read Article &raquo;</a>";
@@ -38,7 +44,7 @@ socket.on('response', function(res){
         else {
             if (fbData[i - 1] != undefined) {
                 col.innerHTML =
-                    '<div class="box"><a style="color:inherit;" onclick="updateArticle(' + i + ')" data-toggle="modal" data-target="#modal" role="button"><img src="' + fbData[i - 1]['picture'].data.url + '" alt="Article picture" class="frontImg">' +
+                    '<div class="box"><a style="color:inherit;" onclick="updateArticle(' + i + ')" data-toggle="modal" data-target="#modal" role="button"><img src="' + res[i - 1].person + '" alt="Article picture" class="frontImg">' +
                     "<h5>" + getHeadline(res[i - 1].title) + "</h5>" +
                     "<div class='colText' >" + getIntro(res[i - 1].body) + "</div></a></div>" //+
                     // "<p><a class='btn btn-default btnInText' onclick='updateArticle(" + i + ")' data-toggle='modal' data-target='#modal' role='button'>Read Article &raquo;</a></p></div>";
@@ -52,8 +58,10 @@ socket.on('response', function(res){
 
         }
 
-
-        articles[i] = {"title": res[i - 1].title, "body": res[i - 1].body};
+        if (res[i - 1].person === undefined)
+            articles[i] = {"title": res[i - 1].title, "body": res[i - 1].body};
+        else
+            articles[i] = {"title": res[i - 1].title, "body": res[i - 1].body, "person": res[i - 1].person};
     }
 
     socket.disconnect();
@@ -77,20 +85,42 @@ function getIntro(article) {
 }
 
 function updateArticle(index) {
+    if (articles[index].person != undefined) {
+        document.getElementById('modalBody').innerHTML=
+            '<img src="' + articles[index].person + '" id="article-image">' +
+            '<p id="article-text">' +
+            formatText(articles[index].body) +
+            '</p>';
+    }
+    else {
+        document.getElementById('modalBody').innerHTML=
+            '<p id="article-text">' +
+            formatText(articles[index].body) +
+            '</p>';
+    }
+
     document.getElementById('article-title').innerText = articles[index].title;
-    document.getElementById('article-text').innerText = formatText(articles[index].body);
 
 }
 
 function formatText(article) {
     var formattedText = "";
 
+    console.log(article);
+
     for (var i = 0; i < article.length; i++) {
         if (article.charAt(i) === '\n')
-            formattedText += '\n\n';
+            if (i + 1 != article.length && article.charAt(i + 1) === '<br>' || article.charAt(i + 1) === '\n')
+                formattedText += '';
+            else
+                formattedText += '<br><br>';
+        else  if (article.charAt(i) === '\t')
+                formattedText += '';
         else
             formattedText += article.charAt(i);
     }
+
+    console.log("hehhehe");
 
     return formattedText;
 }
